9329b192cc84280d9d855c80b1161df4a4e9f107
Tiruum <selintimur@yandex.ru>
2024-01-22 14:39:52 +0300

    Added files

---
+++ config.ts
@@ -0,0 +1,58 @@
+function chartData() {
+    return {
+        datasets: [
+            {
+                label: 'Data chart',
+                data: trainingSet().map((point) => {
+                    return {x:point.humidity, y:point.pressure}
+                }),
+                pointBackgroundColor: trainingSet().map((point) => {
+                    return point.rain ? 'red' : 'blue'
+                }),
+                pointRadius: 4,
+                showLine: false,
+                backgroundColor: 'purple'
+            }
+        ]
+    }
+}
+
+function chartOptions() {
+    return {
+        maintainAspectRatio: false,
+        legend: {
+            labels: {
+                fontSize: 20
+            }
+        },
+        responsive: true,
+        scales: {
+            xAxes: {
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Humidity',
+                        fontSize: 20
+                    },
+                    ticks: {
+                        fontSize: 20,
+                        max: 10,
+                        min: 0
+                    }
+                },
+            yAxes: {
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Pressure',
+                        fontSize: 20
+                    },
+                    ticks: {
+                        fontSize: 20,
+                        max: 10,
+                        min: 0
+                    }
+                },
+        }
+    }
+}
